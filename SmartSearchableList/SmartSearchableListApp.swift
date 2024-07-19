82f1ce019ed32bebadf6a5f08b1171ab08ded2ac
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2024-07-19 13:52:23 +0200

    Init commit

---
+++ SmartSearchableList/SmartSearchableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSearchableListApp.swift
+//  SmartSearchableList
+//
+//  Created by Aleksander Stojanowski
+//
+
+import SwiftUI
+
+@main
+struct SmartSearchableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartSearchableList/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartSearchableList
+//
+//  Created by Aleksander Stojanowski
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

6b0aff8a327391a65e943c5c2cdf2e4af6b85211
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2024-07-21 17:32:33 +0200

    Give user more choices

--- SmartSearchableList/Views/Components/ProductsListing.swift
+++ SmartSearchableList/Views/Components/ProductsListing.swift
@@ -9,11 +9,17 @@ import SwiftData
 import SwiftUI
 
 struct ProductsListing: View {
+    private let nonexistentProduct: Product
     @Query
     private var filteredProducts: [Product]
 
+    private var showNewProduct: Bool {
+        !nonexistentProduct.name.isEmpty && !filteredProducts.contains(where: { $0.name.localizedCaseInsensitiveCompare(nonexistentProduct.name) == .orderedSame })
+    }
+
     init(withName searchText: String) {
         let sanitizedSearchText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        nonexistentProduct = Product(name: sanitizedSearchText)
         _filteredProducts = Query(
             filter: #Predicate {
                 sanitizedSearchText.isEmpty || $0.name.localizedStandardContains(sanitizedSearchText)
@@ -25,10 +31,18 @@ struct ProductsListing: View {
 
     var body: some View {
         List {
-            ForEach(filteredProducts) {
-                ProductTile($0)
+            if showNewProduct {
+                Section {
+                    ProductTile(nonexistentProduct)
+                }
+            }
+            Section {
+                ForEach(filteredProducts) {
+                    ProductTile($0)
+                }
             }
         }
         .animation(.default, value: filteredProducts)
+        .animation(.default, value: nonexistentProduct)
     }
 }
